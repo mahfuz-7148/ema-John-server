33f3ce1f92410aeef2d64b36d912cdf68e198dc9
mahfuz-7148 <mrahman7148@gmail.com>
2025-07-04 20:00:40 +0600

    pagination

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ema-john.ftku5dr.mongodb.net/?retryWrites=true&w=majority&appName=ema-john`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,13 +29,35 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+        const page = Number(req.query.page)
+        const size = Number(req.query.size)
+        // console.log(page, size)
+        const result = await productCollection.find().skip(page * size).limit(size).toArray();
         res.send(result);
     })
 
+      app.get('/productsCount', async (req, res) => {
+          const count = await productCollection.estimatedDocumentCount()
+          res.send({count})
+      })
+      app.post('/productByIds', async (req, res) => {
+          const ids = req.body
+          const idsWithObjectId = ids.map(id => new ObjectId(id))
+          const filter = {
+              _id: {
+                  $in: idsWithObjectId
+              }
+          }
+          const result = await productCollection.find(filter).toArray()
+          res.send(result)
+          // console.log(filter)
+
+
+      })
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
